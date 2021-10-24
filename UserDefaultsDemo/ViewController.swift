b4d80930798ecc0f1a898389fdafbbba53efec95
Maxim Mitin <maximus405@gmail.com>
2021-10-25 00:47:35 +0300

    Initial Commit

---
+++ UserDefaultsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsDemo
+//
+//  Created by Maxim Mitin on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
